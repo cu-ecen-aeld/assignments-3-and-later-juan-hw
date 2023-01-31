63187d88821a18d22c6bdd0ef857422a4fae4573
juan-hw <juan.trujillo@gmx.de>
2023-01-31 01:07:35 +0100

    change of scripts

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
+# Tester script for assignment 1 and assignment 2
+# Author: Siddhant Jajoo
+
 filesdir=$1
 searchstr=$2
-args_num=$# 
 
-#check man test for if-arguments 
-
-if [ ! $args_num -eq 2 ]; then
-	echo "ERROR: Invalid Number of Arguments"
-	echo "Total number of arguments should be 2"
-	echo "The order of the arguments should be:"
-	echo "	1) File Directory Path."
-	echo "	2) String to be searched in the specified directory path."
-	exit 1
+if [ $# -lt 2 ]
+then
+	echo "Invalid number of argument"
+elif [ -d $filesdir ]
+then
+	X=$(ls $filesdir | wc -l)
+	Y=$(grep $searchstr $filesdir/*  | wc -l)
+	echo "The number of files are ${X} and the number of matching lines are ${Y}"
 else
-	if [ -d "$filesdir" ]; then
-		echo "$filesdir is a directory"
-		cd $filesdir 
-		num_files=`sudo find . -type f -print | wc -l`
-		match_lines=`grep -ar "$searchstr" * | wc -l`
-		echo "The number of files are $num_files and the number of matching lines are $match_lines"
-		exit 0
-	else
-		echo "$filesdir does not represent a directory in the filesystem"
-		exit 1
-	fi
+	echo "${filesdir} is not a directory"
 fi
-
-
